ec15e9a75862485ca7d21899741dcef56d3a54a1
Abaku009 <davidszabolcs15@gmail.com>
2024-04-17 14:15:07 +0200

    modifed function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
 function getComputerChoice() {
-    let choices = "Rock , Paper , Scissors";
-    let randomChoices = choices[Math.floor(Math.random() * choices.length)];
+    let choiceOne = "Rock";
+    let choiceTwo = "Paper";
+    let choiceThree = "Scissors";
 
-    console.log(randomChoices);
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if(randomNumber === 1) {
+        console.log(choiceOne);
+    } else if(randomNumber === 2) {
+        console.log(choiceTwo);
+    } else if(randomNumber === 3) {
+        console.log(choiceThree);
+    }
 }
 
 getComputerChoice();
